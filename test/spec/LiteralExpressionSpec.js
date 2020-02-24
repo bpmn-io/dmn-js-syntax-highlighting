a35a8d1b8a0ccf49d1da33268772bff1ccd89994
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-02-24 12:02:37 +0100

    test(literal-expression): change editor selector

--- test/spec/LiteralExpressionSpec.js
+++ test/spec/LiteralExpressionSpec.js
@@ -48,7 +48,7 @@ describe('literal expression', function() {
       '.test-container .dmn-js-parent { height: 500px; }'
     );
 
-    insertCSS('style.css', '.codemirror { height: 100%; overflow: auto; }');
+    insertCSS('style.css', '.textarea > div { height: 100%; overflow: auto; }');
   });
 
   beforeEach(function(done) {
